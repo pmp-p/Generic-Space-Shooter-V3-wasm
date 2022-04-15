d08617fa9f04f40ab9472ccd3460e7d25a4e9985
pmp-p <mail.peny@free.fr>
2022-04-15 22:33:19 +0200

    cleanup

--- src/dark_asteroid.py
+++
@@ -1,9 +0,0 @@
-from asteroid import Asteroid
-
-
-class DarkAsteroid(Asteroid):
-    def __init__(self, image, position, width, height):
-        super().__init__(image, position, width, height)
-
-    def update(self):
-        super().update()
